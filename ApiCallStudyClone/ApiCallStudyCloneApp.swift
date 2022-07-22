b00ad52dcbe380ae4b4e7fbed74fa76b6ff16c4e
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-22 14:16:21 +0300

    Initial Commit

---
+++ ApiCallStudyClone/ApiCallStudyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCallStudyCloneApp.swift
+//  ApiCallStudyClone
+//
+//  Created by Ömer Faruk Kılıçaslan on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ApiCallStudyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
